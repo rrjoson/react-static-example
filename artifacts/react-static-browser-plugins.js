c7d9c027f99e096eb3275c5cf40e48413350db8c
rrjoson <rrjoson08@gmail.com>
2020-02-12 14:53:26 +0800

    Initial commit

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/Users/rrjoson/Downloads/toptal-interview-project-static/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/rrjoson/Downloads/toptal-interview-project-static/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/rrjoson/Downloads/toptal-interview-project-static/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/rrjoson/Downloads/toptal-interview-project-static/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/rrjoson/Downloads/toptal-interview-project-static",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/404.js': require('/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/404.js').default,
+'/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/about.js': require('/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/about.js').default,
+'/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/blog.js': require('/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/blog.js').default,
+'/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/index.js': require('/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/index.js').default,
+'/Users/rrjoson/Downloads/toptal-interview-project-static/src/containers/Post': require('/Users/rrjoson/Downloads/toptal-interview-project-static/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/rrjoson/Downloads/toptal-interview-project-static/src/pages/404.js'
+
